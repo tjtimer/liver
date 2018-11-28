f6f193d964ff731e79757df626ce943358c9dad2
tjtimer <tjtimer@gmail.com>
2018-11-28 06:59:15 +0100

    first drafts

--- liver/app.py
+++ liver/app.py
@@ -3,15 +3,20 @@ app
 author: Tim "tjtimer" Jedro
 created: 28.11.18
 """
+from getpass import getpass
+from pprint import pprint
+
 import jinja2
 import jinja2_sanic
 import yaml
 from sanic import Sanic, response
 
+from auth.service import auth
+
 app = Sanic()
 
 with open('./conf/app.conf.yaml', 'r') as conf:
-    app.config.from_object(yaml.safe_load(conf.read()))
+    app.config.update(**yaml.safe_load(conf.read()))
 
 app.static('/static', app.config.STATIC_DIR)
 
@@ -20,15 +25,18 @@ loader = jinja2.FileSystemLoader(searchpath=[app.config.TEMPLATES_DIR])
 jinja2_sanic.setup(app, loader=loader)
 
 
-@app.route('/', name='index')
+@app.get('/')
 @jinja2_sanic.template('index.html')
 async def index(_):
     return response.html({})
 
+app.blueprint([auth])
+
 
 @app.listener('before_server_start')
 async def setup(app, loop):
-    pass
+    print('setup app')
+    pprint(app.blueprints)
 
 
 @app.listener('after_server_stop')
