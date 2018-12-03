533666190252308b0db56d9e311d5ecc9db5f636
tjtimer <tjtimer@gmail.com>
2018-12-03 21:22:00 +0100

    storage db setup

--- liver/storage/db.py
+++ liver/storage/db.py
@@ -3,17 +3,39 @@ db
 author: Tim "tjtimer" Jedro
 created: 30.11.18
 """
-
-from aio_arango.client import ArangoAdmin
-
-import datetime
-
+import arrow
 import inflect as inflect
-from graphene import ObjectType, ID, String, DateTime, Scalar
+from aio_arango.client import ArangoAdmin
+from aio_arango.db import ArangoDB, DocumentType
+from graphene import ID, ObjectType, Scalar, String
+from graphql.language import ast
 
 ifl = inflect.engine()
 node_registry = set()
 edge_registry = set()
+
+
+class ArrowType(Scalar):
+
+    @staticmethod
+    def serialize(value):
+        try:
+            dt = arrow.get(value)
+            return dt.for_json()
+        except TypeError:
+            return None
+
+    @classmethod
+    def parse_literal(cls, node):
+        if isinstance(node, ast.StringValue):
+            return cls.parse_value(node.value)
+
+    @staticmethod
+    def parse_value(value):
+        try:
+            return arrow.get(value)
+        except ValueError:
+            return None
 
 
 class BaseModel(ObjectType):
@@ -24,8 +46,8 @@ class BaseModel(ObjectType):
         cls._id = ID()
         cls._key = String()
         cls._rev = String()
-        cls._created = DateTime()
-        cls._updated = DateTime()
+        cls._created = ArrowType()
+        cls._updated = ArrowType()
 
     @property
     def collection_name(self)->str:
@@ -37,8 +59,8 @@ class BaseModel(ObjectType):
 
     async def create(self, client):
         data = self.data
-        self._created = datetime.datetime.utcnow()
-        data['_created'] = self._created.isoformat()
+        self._created = arrow.utcnow()
+        data['_created'] = self._created.timestamp
         obj = await client[self.collection_name].add(data)
         self.__dict__.update(**obj)
         return self
@@ -71,7 +93,22 @@ class Edge(BaseModel):
         cls._to = String()
 
 
-async def setup(config: dict):
-    db = ArangoAdmin(*config.DATABASE.split(':'))
+async def setup_admin(config: dict):
+    db = ArangoAdmin(*config.USER.split(':'))
     await db.login()
+    return db
+
+
+async def setup(cli: ArangoAdmin, cfg: dict)->ArangoDB:
+    db_name = cfg['name']
+    cred = cfg['user'].split(':')
+    if db_name not in await cli.get_dbs():
+        await cli.create_db(db_name)
+        await cli.create_user(*cred)
+    db = ArangoDB(*cred, db_name)
+    await db.login()
+    for col_name in cfg['schema']['nodes'].keys():
+        await db.create_collection(col_name)
+    for col_name in cfg['schema']['edges'].keys():
+        await db.create_collection(col_name, DocumentType.EDGE)
     return db
